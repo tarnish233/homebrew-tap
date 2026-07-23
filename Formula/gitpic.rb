f205914deb651658a99063f8788acbda1bb4afaf
tarnish233 <63766688+tarnish233@users.noreply.github.com>
2026-07-23 09:54:54 +0800

    gitpic 0.1.1: bump + auto-install shell completions

--- Formula/gitpic.rb
+++ Formula/gitpic.rb
@@ -1,32 +1,34 @@
 class Gitpic < Formula
   desc "Upload local/clipboard images to a GitHub repo (image host) and get a Markdown link"
   homepage "https://github.com/tarnish233/gitpic-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.0/gitpic-aarch64-apple-darwin.tar.gz"
-      sha256 "f4e7a89e21b2502628a68cff448001ac160d5a75d1731fa749a8be1e690a8b2e"
+      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.1/gitpic-aarch64-apple-darwin.tar.gz"
+      sha256 "6af7d91a55ab508db8b25ea8fdce03ea6dc38700f71dbfffc1212fd960f57ea7"
     end
     on_intel do
-      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.0/gitpic-x86_64-apple-darwin.tar.gz"
-      sha256 "a4033895b6f745d61eb8d664db60d96fea9152f83825d214e313139f9566ad2d"
+      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.1/gitpic-x86_64-apple-darwin.tar.gz"
+      sha256 "ba73ca591190058fad7d9bc30ff7e69c1b3b371753ff2af5d72b9994b295cfc4"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.0/gitpic-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c218c54b930eaa5fc77fbf6ae984dd6c46beea88220a1beec3771e5434e31535"
+      url "https://github.com/tarnish233/gitpic-cli/releases/download/v0.1.1/gitpic-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "31990c40b893bd2217be21b870e84d383f2a9ac17af4aa772d045424301c0e9b"
     end
   end
 
   def install
     bin.install "gitpic"
+    # Auto-generate & install shell completions (bash, zsh, fish)
+    generate_completions_from_executable(bin/"gitpic", "completion")
   end
 
   test do
-    assert_match "gitpic", shell_output("#{bin}/gitpic --version")
+    assert_match "gitpic #{version}", shell_output("#{bin}/gitpic --version")
   end
 end
